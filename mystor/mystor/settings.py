357dd849abb36d29eca5362920fd472d6e077f68
mohammed-MFSA99 <mohammedalamerilaptop@gmail.com>
2025-11-25 03:02:53 +0300

    Update settings.py

--- mystor/mystor/settings.py
+++ mystor/mystor/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -151,3 +152,5 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # الجلسة لا تنتهي عند �
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
